1baaa721802ed1e618818e839d26f44d97deb31b
Jamkris <dltmdgus1412@gmail.com>
2024-04-04 23:51:12 +0900

    ADD :: 기본세팅 완료

---
+++ src/components/header/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as _ from './style'
+
+const Header = () => {
+  return (
+    <_.Header_Container>
+      
+    </_.Header_Container>
+  );
+};
+
+export default Header;
