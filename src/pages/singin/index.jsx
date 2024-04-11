28ddfbfcecc29ed01c2519770a33948290c4b66b
rkdalswl718 <rkdalswl070718@gmail.com>
2024-04-12 02:18:57 +0900

    ADD :: page

---
+++ src/pages/singin/index.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import * as _ from './style';
+import Logo from '../../assets/img/Login_Logo.svg';
+import { useNavigate } from 'react-router-dom';
+
+const SignIn = () => {
+	const history = useNavigate();
+	return (
+		<_.SignIn_Container>
+			<img src={Logo} width='120px' height='120px' alt='Logo' />
+			<_.SiginIn_ID_Input placeholder='아이디를 입력해주세요' />
+			<_.SiginIn_PW_Input placeholder='비밀번호를 입력해주세요' />
+
+			<_.SigIn_Button>로그인</_.SigIn_Button>
+
+			<_.SignIn_NO_Exist>
+				회원이 아니신가요?
+				<p onClick={() => history('/auth/signup')}>회원가입</p>
+			</_.SignIn_NO_Exist>
+		</_.SignIn_Container>
+	);
+};
+
+export default SignIn;
+
---
+++ src/pages/singup/index.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import * as _ from './style';
+import Logo from '../../assets/img/Login_Logo.svg';
+import { useNavigate } from 'react-router-dom';
+
+const SignUp = () => {
+	const history = useNavigate();
+	return (
+		<_.SignIn_Container>
+			<img src={Logo} width='120px' height='120px' alt='Logo' />
+			<_.SiginIn_ID_Input
+				placeholder='아이디를 입력해주세요'
+				type='text'
+			/>
+			<_.SiginIn_PW_Input
+				placeholder='비밀번호를 입력해주세요'
+				type='password'
+			/>
+			<_.SiginIn_EMAIL_Input
+				placeholder='이메일을 입력해주세요'
+				type='email'
+			/>
+			<_.SiginIn_NICKNAME_Input
+				placeholder='닉네임을 입력해주세요'
+				type='text'
+			/>
+
+			<_.SigIn_Button>회원가입</_.SigIn_Button>
+
+			<_.SignIn_NO_Exist>
+				이미 회원이신가요?
+				<p onClick={() => history('/auth/signin')}>로그인</p>
+			</_.SignIn_NO_Exist>
+		</_.SignIn_Container>
+	);
+};
+
+export default SignUp;
