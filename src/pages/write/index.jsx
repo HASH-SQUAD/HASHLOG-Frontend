6362ce27a30bbb6744911b885867261291399ce1
rkdalswl718 <rkdalswl070718@gmail.com>
2024-04-14 15:49:23 +0900

    ADD :: line

--- src/pages/write/index.jsx
+++ src/pages/write/index.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import Header from '../../components/header';
-import {useState} from 'react';
-
+import React from "react";
+import Header from "../../components/header";
+import { useState } from "react";
+import * as _ from "./style";
 
 const Write = () => {
-    const [userName, setUserName] = useState("강민지님");
+  const [userName, setUserName] = useState("강민지님");
 
-    return (
-        <>
+  return (
+    <>
         <Header userName={userName} />
-        
-        
-        </>
-    );
+        <_.Write_Layout>
+            <_.Write_Line/>
+      </_.Write_Layout>
+    </>
+  );
 };
 
 export default Write;
--- src/pages/write/style.js
+++ src/pages/write/style.js
@@ -1,1 +1,15 @@
 import styled from "styled-components";
+
+export const Write_Layout = styled.div`
+  width: 100%;
+  height: 100%;
+`
+
+export const Write_Line = styled.div `
+  width: 100%;
+  border-bottom: 1.5px solid #eeeeee;;
+`
+
+export const Write_Title = styled.input `
+
+`
