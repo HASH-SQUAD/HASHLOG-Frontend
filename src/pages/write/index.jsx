582ddfaba8eda2dc3f96729d8b8dbe216e4d291c
rkdalswl718 <rkdalswl070718@gmail.com>
2024-04-16 21:35:01 +0900

    ADD :: writefooter div

--- src/pages/write/index.jsx
+++ src/pages/write/index.jsx
@@ -2,74 +2,82 @@ import React from "react";
 import Header from "../../components/header";
 import { useState } from "react";
 import * as _ from "./style";
+import "./style.css";
 import ReactQuill from "react-quill";
 
 const Write = () => {
   const [userName, setUserName] = useState("강민지님");
-  const [title, setTitle] = useState("")
+  const [title, setTitle] = useState("");
   const handleTitleChange = (e) => {
-    setTitle(e.target.value);
+    setTitle(e.currentTarget.value);
   };
   const formats = [
-  'font',
-  'header',
-  'bold',
-  'italic',
-  'underline',
-  'strike',
-  'blockquote',
-  'list',
-  'bullet',
-  'indent',
-  'link',
-  'align',
-  'color',
-  'background',
-  'size',
-  'h1',
-];
+    "font",
+    "header",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "align",
+    "color",
+    "background",
+    "size",
+    "h1",
+  ];
   const modules = {
     toolbar: {
       container: [
         ["image"],
         [{ header: [1, 2, 3, 4, 5, false] }],
-        ['font',
-        'header',
-        'bold',
-        'italic',
-        'underline',
-        'strike',
-        'blockquote',
-        'list',
-        'bullet',
-        'indent',
-        'link',
-        'align',
-        'color',
-        'background',
-        'size',
-        'h1',],
+        [
+          "font",
+          "header",
+          "bold",
+          "italic",
+          "underline",
+          "strike",
+          "blockquote",
+          "list",
+          "bullet",
+          "indent",
+          "link",
+          "align",
+          "color",
+          "background",
+          "size",
+          "h1",
+        ],
       ],
     },
   };
   return (
     <>
-        <Header userName={userName} />
-        <_.Write_Layout>
-            <_.Write_Line/>
-            <_.Write_Write>
-            <_.Write_TitleInput
+      <Header userName={userName} />
+        <_.Write_Line />
+      <_.Write_Layout>
+        <_.Write_Write>
+          <_.Write_TitleInput
             type="text"
-            value={title} maxLength='50'
+            value={title}
+            maxLength="50"
             onChange={handleTitleChange}
             placeholder="제목을 입력하세요"
-          />
+            />
           <ReactQuill
             style={{ width: "100%", height: "100%" }}
             modules={modules}
-          />
-          </_.Write_Write>
-        </_.Write_Layout>
+            />
+            <_.Write_footer></_.Write_footer>
+        </_.Write_Write>
+        <_.Write_View>
+          
+        </_.Write_View>
+      </_.Write_Layout>
     </>
   );
 };
--- src/pages/write/style.js
+++ src/pages/write/style.js
@@ -2,37 +2,46 @@ import styled from "styled-components";
 
 export const Write_Layout = styled.div`
   width: 100%;
-  height: 100%;
-`
+  height: 100vh;
+  display: flex;
+  flex-direction: row;
+`;
 
-export const Write_Line = styled.div `
+export const Write_Line = styled.div`
   width: 100%;
-  border-bottom: 1.5px solid #eeeeee;;
-`
+  border-bottom: 1.5px solid #eeeeee;
+`;
 
 export const Write_Write = styled.div`
+  /* overflow: scroll; */
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 50vw;
+  width: 50%;
   height: 100%;
   float: left;
   flex-direction: column;
 `;
 
-
-export const Write_TitleInput = styled.textarea `
+export const Write_TitleInput = styled.textarea`
   width: 90%;
   border-style: none;
   outline: none;
   padding: 30px 0 20px 20px;
   font-size: 3rem;
   resize: none;
-  word-spacing:-20px;
-
-`
+  word-spacing: -20px;
+`;
 export const Write_View = styled.div`
-  width: 50vw;
-  background-color: #F5F5F5;
+  width: 50%;
+  height: 100%;
+  background-color: #f5f5;
   float: right;
-`
+`;
+
+export const Write_footer = styled.div`
+  width: 100%;
+  height: 10%;
+  box-shadow: #d9d9d9 0px 0px 8px 0;
+  bottom: 0;
+`;
