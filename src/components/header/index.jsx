0e7ee207233cb16d551c08a0bbf1f38183a26e2a
rkdalswl718 <rkdalswl070718@gmail.com>
2024-06-10 04:20:16 +0900

    CHORE :: 오타 수정

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,23 +2,21 @@ import React, { useState } from "react";
 import * as _ from "./style";
 import { Link, useNavigate } from "react-router-dom";
 
-const Header = () => {
-  const navigate = useNavigate()
-  const userName = 'minji';
+const Header = ({userName}) => {
   const [isLogined, setIsLogined] = useState(false);
+  const history = useNavigate()
   return (
     <_.Header_Container>
-      <Link to="/"><h1>#</h1></Link>
+      <h1>#</h1>
       {isLogined ? (
         userName ? (
           <_.Header_button>{userName}</_.Header_button>
         ) : (
           <_.Header_button><Link to="/write">글쓰기</Link></_.Header_button>
         )
-      ) : 
-        <_.Header_button backgroundColor="#fff" color="#696969" onClick={() => navigate('/auth/signin')}>
-          로그인</_.Header_button>
-        }
+      ) : (
+        <_.Header_button backgroundColor="#fff" color="#696969"><Link to="/auth/singin">로그인</Link></_.Header_button>
+      )}
       </_.Header_Container>
   );
 };
