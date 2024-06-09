1ac7250f7bc26a5feeed4ef005f8e53602253402
rkdalswl718 <rkdalswl070718@gmail.com>
2024-06-10 03:38:45 +0900

    FIX :: 삼항연산자

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -2,22 +2,20 @@ import React, { useState } from "react";
 import * as _ from "./style";
 import { Link, useNavigate } from "react-router-dom";
 
-const Header = ({userName}) => {
-  const [isLogined, setIsLogined] = useState(false);
+const Header = () => {
   const navigate = useNavigate()
   return (
     <_.Header_Container>
       <Link to="/"><h1>#</h1></Link>
-      {isLogined ? (
         userName ? (
-          <_.Header_button>{userName}</_.Header_button>
-        ) : (
+          <>
+          <_.Header_button>유저내임드드</_.Header_button>
           <_.Header_button><Link to="/write">글쓰기</Link></_.Header_button>
-        )
-      ) : 
+          </>
+      ) : (
         <_.Header_button backgroundColor="#fff" color="#696969" onClick={() => navigate('/auth/signin')}>
           로그인</_.Header_button>
-        }
+          )
       </_.Header_Container>
   );
 };
