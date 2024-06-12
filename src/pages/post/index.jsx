8dbeb201f1fc8789fb2a96e878370260230efe27
rkdalswl718 <rkdalswl070718@gmail.com>
2024-06-13 04:27:18 +0900

    ADD :: post content

--- src/pages/post/index.jsx
+++ src/pages/post/index.jsx
@@ -1,31 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../../components/header";
 import Footer from "../../components/footer";
 import * as _ from "./style";
 import img from "../../assets/Pinterest Image.jpg";
+import shareB from "../../assets/shareB.svg";
+import shareW from "../../assets/shareW.svg";
 
 const Post = () => {
-  const { title, date, tags, image } = {
+  const data = {
     title: "해쉬브라운 먹고싶다",
     date: "2023.09.02",
     tags: ["일상", "사담"],
-    image: { img },
+    content: `해쉬브라운을 먹고 싶은 이유는 여러 가지가 있습니다. 먼저, 해쉬브라운은 감자와 버터의 조화로 고소하고 바삭한 맛을 자랑합니다. 잘 튀겨진 해쉬브라운의 바삭한 겉면과 부드러운 속은 누구나 좋아할 만한 맛을 제공합니다.
+
+    또한, 해쉬브라운의 바삭바삭한 식감은 입안에서 즐거움을 줍니다. 감자의 부드러움과 겉의 바삭함이 조화를 이루어 씹는 즐거움을 배가시킵니다. 아침 식사로 해쉬브라운을 먹으면 든든하고 기분 좋은 시작을 할 수 있습니다.
+    
+    해쉬브라운은 다양한 방식으로 즐길 수 있어 더 매력적입니다. 간단하게 케첩이나 소스를 곁들여 먹거나, 계란과 베이컨을 곁들여 더욱 풍성한 식사를 만들 수 있습니다. 이러한 다양한 조합은 해쉬브라운을 더욱 먹고 싶게 만듭니다.
+    
+    마지막으로, 해쉬브라운은 편리한 간식이기도 합니다. 냉동 해쉬브라운을 이용하면 언제든지 간편하게 만들어 먹을 수 있어 바쁜 일상 속에서도 쉽게 즐길 수 있습니다. 이러한 이유들로 인해 해쉬브라운은 언제나 먹고 싶은 음식 중 하나로 손꼽힙니다.`,
   };
+
+  const [copySuccess, setCopySuccess] = useState(false);
+
+  const copyUrlToClipboard = () => {
+    const url = window.location.href;
+    navigator.clipboard.writeText(url).then(() => {
+      setCopySuccess(true);
+      setTimeout(() => {
+        setCopySuccess(false);
+      }, 2000);
+    });
+  };
+
   return (
     <>
       <Header />
       <_.Post_Layout>
-        <_.Post_Title>{title}</_.Post_Title>
+        <_.Post_Title>{data.title}</_.Post_Title>
         <_.Post_Meta>
-          <_.Post_Tag>{tags}</_.Post_Tag>
-          <_.Post_Date>{date}</_.Post_Date>
+          {data.tags.map((tag, index) => (
+            <_.Post_Tag key={index}>{tag}</_.Post_Tag>
+          ))}
+          <_.Post_Date>{data.date}</_.Post_Date>
         </_.Post_Meta>
-        <_.Post_Title_Line></_.Post_Title_Line>
-        <_.Post_Content>
-        <_.Post_Img src={img}></_.Post_Img>
-        </_.Post_Content>
+        <_.Post_Title_Line />
+        <_.Post_Img_Layout>
+          <_.Post_Img src={img} />
+        </_.Post_Img_Layout>
+        <_.Post_Content_Layout>
+          <_.Post_Content>{data.content}</_.Post_Content>
+          <_.Post_Share_Button onClick={copyUrlToClipboard}>
+            <_.Post_Share_Icon src={shareW} />
+            <p>공유하기</p>
+          </_.Post_Share_Button>
+        </_.Post_Content_Layout>
       </_.Post_Layout>
       <Footer />
+      {copySuccess && (
+        <_.Post_Share_Success>링크가 복사되었습니다.</_.Post_Share_Success>
+      )}
     </>
   );
 };
--- src/pages/post/style.js
+++ src/pages/post/style.js
@@ -4,7 +4,7 @@ export const Post_Layout = styled.div `
   overflow: auto;
   width: 100vw;
   background-color: #FBFBFD;
-  padding: 0 200px 200px 200px;
+  padding: 0 200px 150px 200px;
   margin: 0;
 `
 
@@ -20,26 +20,28 @@ export const Post_Title = styled.div `
 export const Post_Meta = styled.div`
   display: flex;
   align-items: center;
-  gap: 10px;
+  gap: 5px;
   margin-top: 1%;
 `
 
 
 export const Post_Tag = styled.div`
   padding: 0.75% 1.25%;
-  width: 10%;
+  width: 6.5%;
   font-size: 0.8rem;
   text-align: center;
   color: #7A7D85;
   background-color: #eee;
   border-radius: 20px;
   border: none;
+
 `;
 
 
 export const Post_Date = styled.div `
   color: #7A7D85;
   font-size: 0.8rem;
+  margin-left: 5px;
 `
 export const Post_Title_Line = styled.div `
   width: 100%;
@@ -47,16 +49,56 @@ export const Post_Title_Line = styled.div `
   background-color:#d9d9d9;
   margin-top: 2%;
   `
-  export const Post_Content = styled.div `
+  export const Post_Img_Layout = styled.div `
     display: flex;
     justify-content: center;
   `
+    
+      export const Post_Img = styled.img `
+      width: 80%;
+      height: 300px;
+      margin-top: 4%;
+      border-radius: 20px;
+      display: block;
+    `
+  export const Post_Content = styled.div`
+    margin-top: 4%;
+    color: #12182b;
+    font-size: 1rem;
+    line-height: 1.6;
+    white-space: pre-wrap;
+  `
 
-  export const Post_Img = styled.img `
-  width: 80%;
-  height: 300px;
-  margin-top: 4%;
-  border-radius: 20px;
-  display: block;
-`
+  export const Post_Content_Layout = styled.div `
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    display: flex;
+  `
+  export const Post_Share_Button = styled.button `
+    display: flex;
+    padding: 10px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 15px;
+    background: #000;
+    width: 14vw;
+    height: 8vh;
+    font-size: 18px;
+    color: #fff;  
+    gap: 2px;
+    margin-top: 50px;
+  `
+  export const Post_Share_Icon = styled.img `
+    background: #000;
+    width: 26px;
+  `
+  export const Post_Share_Success = styled.img `
+    background: #FFF;
+    width: 20vw;
+    height: 8vh;
+  `
+
+
+
 
